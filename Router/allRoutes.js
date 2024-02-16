059c4c07dd664a7c9dbbb8d831d85913a01d626e
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-02-16 17:16:42 +0100

    galerie

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -1,9 +1,9 @@
-import Route from "./route.js";
+import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/galerie", "Galerie", "/pages/galerie.html")
+    new Route("/galerie", "Galerie", "/pages/galerie.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
