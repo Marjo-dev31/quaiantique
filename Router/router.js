0770bde6d9c9f719168537fe8a08a0a1195e478e
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-03-05 13:55:23 +0100

    probleme routage

--- Router/router.js
+++ Router/router.js
@@ -2,7 +2,7 @@ import Route from "./Route.js";
 import { allRoutes, websiteName } from "./allRoutes.js";
 
 // Création d'une route pour la page 404 (page introuvable)
-const route404 = new Route("404", "Page introuvable", "/pages/404.html");
+const route404 = new Route("/404", "Page introuvable", "/pages/404.html");
 
 // Fonction pour récupérer la route correspondant à une URL donnée
 const getRouteByUrl = (url) => {
@@ -24,9 +24,10 @@ const getRouteByUrl = (url) => {
 // Fonction pour charger le contenu de la page
 const LoadContentPage = async () => {
   const path = window.location.pathname;
-  console.log(path)
+  console.log(path, 'toto')
   // Récupération de l'URL actuelle
   const actualRoute = getRouteByUrl(path);
+  console.log(actualRoute, 'titi')
   // Récupération du contenu HTML de la route
   const html = await fetch(actualRoute.pathHtml).then((data) => data.text());
   // Ajout du contenu HTML à l'élément avec l'ID "main-page"
@@ -48,19 +49,35 @@ const LoadContentPage = async () => {
   
 };
 
+const link = document.getElementById('galerieLink');
+
+// links.forEach((element) => {
+  link.addEventListener('click', (event)=> {
+    event.preventDefault(); 
+    console.log(link)
+    console.log(allRoutes, 'tata')
+    console.log(window.location.href, 'ruru')
+    // window.location.href = event.currentTarget.url
+  //  if (Route.url === event.currentTarget.href) {
+  //   window.location.href = event.currentTarget.href
+  //  }
+  LoadContentPage();
+    console.log(getRouteByUrl, 'riri')
+  })
+  
+  const routeEvent = (event) => {
 // Fonction pour gérer les événements de routage (clic sur les liens)
-const routeEvent = (event) => {
-  event = event || window.event;
-  event.preventDefault();
+  // event = event || window.event;
   // Mise à jour de l'URL dans l'historique du navigateur
   window.history.pushState({}, "", event.target.href);
   // Chargement du contenu de la nouvelle page
   LoadContentPage();
 };
-
+// 
 // Gestion de l'événement de retour en arrière dans l'historique du navigateur
 window.onpopstate = LoadContentPage;
 // Assignation de la fonction routeEvent à la propriété route de la fenêtre
 window.route = routeEvent;
+
 // Chargement du contenu de la page au chargement initial
 LoadContentPage();
