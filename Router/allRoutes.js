3ddaa147de6208ed298c65d0b5d8153a4bfd1e50
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2024-03-08 14:57:26 +0100

    input nom prenom mail verifie

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,11 +5,11 @@ export const allRoutes = [
     new Route("/home", "Accueil", "/pages/home.html"),
     new Route("/galerie", "Galerie", "/pages/galerie.html"),
     new Route("/signin", "Connexion", "/pages/auth/signin.html"),
-    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/", "Inscription", "/pages/auth/signup.html","/js/auth/signup.js"),
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
     new Route("/editPassword", "Changer le mot de passe", "/pages/auth/editPassword.html"),
     new Route("/allResa", "Vos réservations", "/pages/reservations/allResa.html"),
-    new Route("/", "Réserver", "/pages/reservations/reserver.html"),
+    new Route("/reserver", "Réserver", "/pages/reservations/reserver.html"),
 
 
 ];
